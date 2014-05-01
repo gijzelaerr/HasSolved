2014073d4fd1f2eac615a1c61b4f73f6e3dd1317
Gijs Molenaar <gijs@pythonic.nl>
2014-05-01 13:11:21 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 HasSolved
 =========
 
-Rubiks cube solver written in Haskell
+Rubiks cube solver written in Haskell.
+
+Plan is to implement IDA\* as a sort of mental challenge.
+
+http://en.wikipedia.org/wiki/IDA*
---
+++ main.hs
@@ -0,0 +1,23 @@
+
+let w = "white"
+let b = "blue"
+let o = "orange"
+let g = "green"
+let r = "red"
+let y = "yellow"
+
+let u = "up"
+let d = "down"
+let f = "front"
+let x = "back"
+let l = "left"
+let t = "right"
+
+let colors = ("white", "blue", "orange", "green", "red", "yellow")
+let sides = ("up", "down", "front", "back", "left", "right")
+let axes = ("x", "y", "z")
+
+let side_order = (u, d, f, x, l, t)
+
+let positions = ( (u, f, l), (u, x, l), (u, f, t), (u, x, t), (d, f, l), (d, x, l), (d, f, t), (d, x, t), (u, f), (u, x), (u, l), (u, t), (d, f), (d, x), (d, l), (d, t)) 
+
